720aed4991345c79f9e0453702b707989b0cc8e3
John Willman <johnpwillman@gmail.com>
2014-11-26 01:37:20 +0000

    Add helper function to display current controller#action and location of view to application.html.erb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  
+  #Show controller and action of current page
+  def controlleraction
+    params[:controller] + "#" + params[:action] + " (app/views/" + params[:controller] + "/" + params[:action] + ".html.erb)"
+  end
+  
 end
